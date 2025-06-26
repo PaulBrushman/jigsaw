8c72d63323a2bfce0cac9ed8f956c6d6f28613ae
Paul Brushman <PaulBrushman@proton.me>
2025-06-26 07:26:09 +0500

    actual multiplication

--- src/cuda/wmma.cu
+++ src/cuda/wmma.cu
@@ -2,7 +2,7 @@
 
 using namespace nvcuda;
 extern "C" __global__ void test_wmma(half *a, half *b, half *c, const size_t stride) {
-   wmma::fragment<wmma::matrix_a, 16, 16, 16, half, wmma::row_major> a_frag;
+   wmma::fragment<wmma::matrix_a, 16, 16, 16, half, wmma::col_major> a_frag;
    wmma::fragment<wmma::matrix_b, 16, 16, 16, half, wmma::row_major> b_frag;
    wmma::fragment<wmma::accumulator, 16, 16, 16, half> dest_frag;
 
@@ -12,5 +12,5 @@ extern "C" __global__ void test_wmma(half *a, half *b, half *c, const size_t str
 
    wmma::mma_sync(dest_frag, a_frag, b_frag, dest_frag);
 
-   wmma::store_matrix_sync(c, dest_frag, stride, wmma::mem_row_major);
+   wmma::store_matrix_sync(c, dest_frag, stride, wmma::mem_col_major);
 }
--- src/zig/root.zig
+++ src/zig/root.zig
@@ -19,8 +19,8 @@ pub fn MMA(a: []f16, b: []f16, side_size: u32, alloc: std.mem.Allocator) ![]f16 
     const n_blocks = side_size * side_size / 256;
     const side_blocks = side_size / 16;
     for (0..n_blocks) |n| {
-        const a_line = n / side_blocks;
-        const b_line = n % side_blocks;
+        const a_line = n % side_blocks;
+        const b_line = n / side_blocks;
         for (0..side_blocks) |i| {
             const a_offset = a_line * 16 + i * side_size * 16;
             const b_offset = b_line * 16 + i * side_size * 16;
@@ -38,24 +38,64 @@ pub fn MMA(a: []f16, b: []f16, side_size: u32, alloc: std.mem.Allocator) ![]f16 
 
 test "single block ones multiply" {
     const alloc = std.testing.allocator;
-    var cok = [_]f16{1} ** 256;
-    const e = try MMA(&cok, &cok, 16, alloc);
+    var mat = [_]f16{1} ** 256;
+    const e = try MMA(&mat, &mat, 16, alloc);
     defer alloc.free(e);
     for (e) |r| assert(r == 16.0);
 }
 
 test "4 block ones multiply" {
     const alloc = std.testing.allocator;
-    var cok = [_]f16{1} ** 1024;
-    const e = try MMA(&cok, &cok, 32, alloc);
+    var mat = [_]f16{1} ** (256 * 4);
+    const e = try MMA(&mat, &mat, 32, alloc);
     defer alloc.free(e);
     for (e) |r| assert(r == 32.0);
 }
 
 test "9 block ones multiply" {
     const alloc = std.testing.allocator;
-    var cok = [_]f16{1} ** (256 * 9);
-    const e = try MMA(&cok, &cok, 48, alloc);
+    var mat = [_]f16{1} ** (256 * 9);
+    const e = try MMA(&mat, &mat, 48, alloc);
     defer alloc.free(e);
     for (e) |r| assert(r == 48.0);
+}
+
+test "actual multiplication" {
+    const alloc = std.testing.allocator;
+    var a = [_]f16{0} ** 256;
+    a[0] = 1;
+    a[8] = 2;
+    a[128] = 3;
+    a[136] = 4;
+    var b = [_]f16{0} ** 256;
+    b[0] = 5;
+    b[8] = 6;
+    b[128] = 7;
+    b[136] = 8;
+    const e = try MMA(&a, &b, 16, alloc);
+    defer alloc.free(e);
+    assert(e[0] == 26);
+    assert(e[8] == 38);
+    assert(e[128] == 30);
+    assert(e[136] == 44);
+}
+
+test "actual multiplication 2" {
+    const alloc = std.testing.allocator;
+    var a = [_]f16{0} ** (256 * 4);
+    a[0] = 1;
+    a[16] = 2;
+    a[512] = 3;
+    a[528] = 4;
+    var b = [_]f16{0} ** (256 * 4);
+    b[0] = 5;
+    b[16] = 6;
+    b[512] = 7;
+    b[528] = 8;
+    const e = try MMA(&a, &b, 32, alloc);
+    defer alloc.free(e);
+    assert(e[0] == 26);
+    assert(e[16] == 38);
+    assert(e[512] == 30);
+    assert(e[528] == 44);
 }
